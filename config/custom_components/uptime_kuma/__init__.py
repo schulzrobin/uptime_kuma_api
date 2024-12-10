c6e2385b3379ca6b3b495aa0a2e8c9e9afcfffcd
schulzrobin <106838867+schulzrobin@users.noreply.github.com>
2024-12-10 21:09:04 +0100

    Aktualisieren von

---
+++ config/custom_components/uptime_kuma/__init__.py
@@ -0,0 +1,23 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Uptime Kuma integration."""
+    hass.data[DOMAIN] = {}
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Uptime Kuma from a config entry."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
